c22265b71e22fb3010a67f695cc812c8f15a303a
liangchengnan <liangchengnan@zcmall.com>
2017-06-27 14:02:08 +0800

    Initial Commit

---
+++ ZCMEvent/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZCMEvent
+//
+//  Created by 梁城楠 on 2017/6/27.
+//  Copyright © 2017年 YD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
